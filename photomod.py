9219cf35186aa3dc34cbff7fdc304db12821add0
lukcic <lukcic@int.pl>
2021-10-31 18:54:01 +0000

    Real creation time saved and added autoinstall packages.

--- photomod.py
+++ photomod.py
@@ -4,23 +4,39 @@ TODO:
 add path as program argument sys.argv
 add walking on subfolders
 add statiscics (log)
-add or autoinstall send2trash and PIllow
 add mp4 support regexJPG = re.compile(r'(\S+\.jpg)|(\S+\.mp4)')
 add GUI and options
 '''
 
-import os, re, time, shutil, hashlib, send2trash
-from PIL import Image                                       #sudo pip3 install --upgrade Pillow
+import os, re, time, shutil, hashlib, subprocess, sys       #import standard python packages used in program
 
+def install_package(package):                               #function that install packages not included to standard python libraries 
+    import importlib                                        
+    try:                                                    #try import package
+        importlib.import_module(package)
+    except ImportError:                                     #if failed, import pip and use it to install given package
+        import pip
+        pip.main(['install', package])
+
+install_package('send2trash')
+install_package('Pillow')
+from PIL import Image  
 
 def resizeJPG(file, ratio):
     image = Image.open(file)                                #creating (opening) image object
+    
+    fileStats = os.stat(file)                               #get file statistics and save it to 'stat_result' object
+    modificationTime = fileStats.st_mtime                   #save modification time (in Windows thats picture real creation time) in variable to set it after resizing
+    #print('Modification time: ' + time.strftime('%Y%m%d_%H%M%S', time.localtime(modificationTime)))
+
     size = image.size                                       #get image size in pixels (return tuple)
-    widthPX = size[0]
-    heightPX = size[1]
-    image = image.resize((int(widthPX / (1/ratio)), int(heightPX / (1/ratio))))
-    image.save(file)
+    widthPX = size[0]                                       #save width from tupple to variable
+    heightPX = size[1]                                      #save height from tupple to variable
+    image = image.resize((int(widthPX / (1/ratio)), int(heightPX / (1/ratio)))) #resize image object with given ratio (widht/2 == width/(1/0.5))
+    image.save(file)                                        #save resized object to file
+    os.utime(file, (modificationTime, modificationTime))    #update modification time from 'now' to old modification time (real image creation time in Win) from variable, os.utime(file, (acess_time, mod_time))
     print(os.path.basename(file) + ' resized from: ' + str(widthPX) + ' x ' + str(heightPX) + ' to: ' + str(image.size[0]) + ' x ' + str(image.size[1]))
+
 
 def removeDuplicates(fileList):
     hashList = []                                           #creating empty list for file hash strings
@@ -57,12 +73,18 @@ def rename(fileList):
 def renameAndCopy(fileList, folder):                                                                                                            #firsth argument is filelist and second is subfolder name where files will be copied with new names
     for file in fileList:
         if os.path.isdir(file):
-            continue        
+            continue
+        
+        fileStats = os.stat(file)
+        modificationTime = fileStats.st_mtime
+        #print(time.strftime('%Y%m%d_%H%M%S', time.localtime(modificationTime)))
+
         fileSize = str(os.path.getsize(file))                                                                                                   
         newFileName = str(time.strftime('%Y%m%d_%H%M%S', time.localtime(int(os.path.getmtime(file))))) + '_' + fileSize.zfill(10) + ".jpg"      
         destFile = os.path.join(os.getcwd(), folder, newFileName)                                                                               #creating new filename full path, that include subdirectory given as second argument
         print(os.path.basename(file) + ' renamed to: ' + '/' + folder + '/' + os.path.basename(destFile))
-        shutil.copy(file, destFile)                                                                                                             #copying file with new name 
+        shutil.copy(file, destFile)
+        os.utime(destFile, (modificationTime, modificationTime))                                                                                                             #copying file with new name 
 
 os.chdir(os.getcwd())                                   #change work directory to current directowy
 
--- videomod.py
+++ videomod.py
@@ -20,5 +20,3 @@ clip = moviepy.editor.VideoFileClip("VID_20211016_091124.mp4")
 clip_resized = clip.resize(height=720)                      # make the height 360px ( According to moviePy documenation The width is then computed so that the width/height ratio is conserved.)
 clip_resized = clip.rotate(90)
 clip_resized.write_videofile("movie_resized.mp4")           #clip_resized = clip.resize(newsize=(my_width,my_height))
-
-
