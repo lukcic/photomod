79ff4e672f851101027f234c7265ccf5fd6182a9
lukcic <lukcic@int.pl>
2021-11-07 16:50:04 +0000

    copying_source.py added

---
+++ copying_source.py
@@ -0,0 +1,50 @@
+#add saving hashes and filenames as dictionary, moving duplicate source files with copy of file to separate dir
+
+import os, hashlib, send2trash, shutil, time
+
+fullFileList = []
+for root, dirs, files in os.walk("\\\qnap\home\Foto Full Res\Różne", topdown=True):
+
+    for name in files:
+        if name.lower().endswith('jpg') or \
+        name.lower().endswith('mp4') or \
+        name.lower().endswith('mts') or \
+        name.lower().endswith('3gp'):
+            fullFileList.append(str(os.path.join(root, name)))
+            print(os.path.join(root, name))
+        else:
+            print('Not a .jpg or .mp4! ' + name)
+            continue
+print(fullFileList)
+
+
+
+if not os.path.exists('D:\\photomod_source\\duplicates'):
+    os.makedirs('D:\\photomod_source\\duplicates')
+
+hashList = []
+ 
+for  item in fullFileList:
+    tempHash = (hashlib.md5(open(item, 'rb').read()).hexdigest())
+    
+    if tempHash in hashList:
+        print('Duplicate detected! '+item)
+        
+        destFileDup = os.path.join('D:\\', 'photomod_source', 'duplicates' , os.path.basename(item))
+        destFileDup.lower()
+        shutil.copy(item, destFileDup)
+
+    else:
+        hashList.append(tempHash)
+
+        fileStats = os.stat(item)
+        modificationTime = fileStats.st_mtime
+        year = time.strftime('%Y', time.localtime(modificationTime))
+
+        if not os.path.exists(os.path.join('D:\\', 'photomod_source', year)):
+            os.makedirs(os.path.join('D:\\', 'photomod_source', year))
+
+        destFile = os.path.join('D:\\', 'photomod_source', year , os.path.basename(item))
+        destFile = destFile.lower()
+        shutil.copy(item, destFile)
+        os.utime(destFile, (modificationTime, modificationTime))
--- videomod.py
+++ videomod.py
@@ -1,5 +1,51 @@
 import subprocess, sys, os, time, shutil
 
+'''
+Codec to use for image encoding. Can be any codec supported
+by ffmpeg. 
+https://en.wikipedia.org/wiki/FFmpeg#Supported_codecs_and_formats
+
+If the filename is has extension '.mp4', '.ogv', '.webm',
+the codec will be set accordingly, but you can still set it if you
+don't like the default. For other extensions, the output filename
+must be set accordingly.
+
+Some examples of codecs are:
+
+``'libx264'`` (default codec for file extension ``.mp4``)
+makes well-compressed videos (quality tunable using 'bitrate').
+
+``'mpeg4'`` (other codec for extension ``.mp4``) can be an alternative
+to ``'libx264'``, and produces higher quality videos by default.
+
+``'rawvideo'`` (use file extension ``.avi``) will produce
+a video of perfect quality, of possibly very huge size.
+
+``png`` (use file extension ``.avi``) will produce a video
+of perfect quality, of smaller size than with ``rawvideo``.
+
+``'libvorbis'`` (use file extension ``.ogv``) is a nice video
+format, which is completely free/ open source. However not
+everyone has the codecs installed by default on their machine.
+
+``'libvpx'`` (use file extension ``.webm``) is tiny a video
+format well indicated for web videos (with HTML5). Open source.
+
+https://zulko.github.io/moviepy/_modules/moviepy/video/VideoClip.html
+final_render.write_videofile('./videos/output_video.mp4', fps = 30, threads = 1, codec = "libx264")
+
+rotate left:
+C:\Users\lukcic\Desktop\ffmpeg-2021-11-03-git-08a501946f-full_build\bin\ffmpeg.exe  -i output4.mp4 -vf "transpose=2" output44.mp4
+0=90CounterCLockwise and Vertical Flip  (default) 
+1=90Clockwise 
+2=90CounterClockwise 
+3=90Clockwise and Vertical Flip
+
+
+MTS:
+C:\Users\lukcic\Desktop\ffmpeg-2021-11-03-git-08a501946f-full_build\bin\ffmpeg.exe -i 20130608_185224_167018496.MTS -vf scale=1920:1080 -preset slow -crf 18 output3.mp4
+'''
+
 def install_package(package):
     import importlib
     try:
